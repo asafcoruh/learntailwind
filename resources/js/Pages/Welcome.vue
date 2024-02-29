6a9d4df00eb8e1ed82185d979daba45915906d30
asaf <asafcoruh2024@gmail.com>
2024-02-29 12:25:11 +0300

    learn

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+
+</script>
+
+<template>
+    <Head title="Welcome" />
+        <div class="flex items-center p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-x-4 mt-12">
+
+        </div>
+
+</template>
